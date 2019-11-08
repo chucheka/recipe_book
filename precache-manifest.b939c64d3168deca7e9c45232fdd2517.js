d90ab61c36a91065cdc865aed5229e067faddf0b
ucheka22 <ryanucheka@gmail.com>
2019-11-08 04:48:56 +0100

    Updates

---
+++ precache-manifest.b939c64d3168deca7e9c45232fdd2517.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cd4c4aed6d80efaba491a394c3ab109",
+    "url": "/recipe_book/index.html"
+  },
+  {
+    "revision": "1bb5103be038e337511f",
+    "url": "/recipe_book/static/css/2.eac1ab5b.chunk.css"
+  },
+  {
+    "revision": "f2b93f2588fb2cc0b96d",
+    "url": "/recipe_book/static/css/main.ea3291a2.chunk.css"
+  },
+  {
+    "revision": "1bb5103be038e337511f",
+    "url": "/recipe_book/static/js/2.ef7267a5.chunk.js"
+  },
+  {
+    "revision": "f2b93f2588fb2cc0b96d",
+    "url": "/recipe_book/static/js/main.5f04b39c.chunk.js"
+  },
+  {
+    "revision": "ad2cfae2ec4f580ae7cf",
+    "url": "/recipe_book/static/js/runtime~main.6f3c7684.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/recipe_book/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/recipe_book/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/recipe_book/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/recipe_book/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/recipe_book/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
